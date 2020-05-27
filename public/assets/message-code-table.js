39b35fb6acbfaa808fdbe6605163a47694f2c422
Chi Yang <chiyang1118@gmail.com>
2020-05-27 10:04:58 +0000

    Push client codes only at Wed May 27 10:04:57 UTC 2020

--- public/assets/message-code-table.js
+++ public/assets/message-code-table.js
@@ -5,17 +5,11 @@ const messageCodes = {
     'default': { name: 'Default message', type: 'default', details: 'This is a document.' },
     '-2': { name: 'unknown errors', type: 'error', details: '' },
     '-1': { name: 'HTTPService error', type: 'error', details: 'Server is currently down. Please try again latter.' },
-    '0': {
-        name: 'Database error', type: 'error',
-        details: 'OOPS! Looks like our database has some errors. Please try again.' +
-        'If this error keeps occurring, please contact the web administrator. Thank you.'
-    },
+    '0': {name: 'Database error', type: 'error', details: 'OOPS! Looks like our database has some errors. Please try again. If this error keeps occurring, please contact the web administrator. Thank you.'},
     /* Server system related */
     's001': {name: 'Server file system error', type: 'error', details: 'The file system on the server went wrong. Please contact the web admin.'},
     's002': { name: 'HTTPService error', type: 'error', details: 'Server is currently down. Please try again latter.' },
-    's003': { name: 'Database error', type: 'error', details: 'OOPS! Looks like our database has some errors. Please try again.' +
-        'If this error keeps occurring, please contact the web administrator. Thank you.'
-    },
+    's003': { name: 'Database error', type: 'error', details: 'OOPS! Looks like our database has some errors. Please try again. If this error keeps occurring, please contact the web administrator. Thank you.'},
     's004': { name: 'Invalid npm package name', type: 'error', details: 'The provided npm package name is invalid.'},
     // Account related
     'a001': { name: 'The email has been used', type: 'error', details: '' },
@@ -28,15 +22,10 @@ const messageCodes = {
     'a008': { name: 'Account already verified', type: 'warning', details: 'This email has already been verified. ' +
         'You may sign in to continue your work.' },
     'a009': { name: 'Sign in failed', type: 'error', details: 'We couldn\'t sign you in. Sure you had the right credentials?' },
-    'a010': { name: 'Signed in', type: 'success',
-        details: 'Hi, you are now signed in. You may create a project to upload analyze your data,' +
-        'or continue to previous works.'
-    },
-    'a011': { name: 'Sign-in required', type: 'warning',
-        details: 'Please sign in first, thank you.' },
+    'a010': { name: 'Signed in', type: 'success', details: 'Hi, you are now signed in. You may create a project to upload analyze your data, or continue to previous works.'},
+    'a011': { name: 'Sign-in required', type: 'warning', details: 'Please sign in first, thank you.' },
     'a012': { name: 'reCaptcha challenge failed', type: 'error', details: 'We do not accept a robot to perform such actions.'},
-    'a013': {name: 'Auth token generation failed', type: 'error',
-        details: 'Sorry, we could not generate an authentication token for you. Please try again. If this error message keeps showing, please contact the administrator. Thank you.'},
+    'a013': {name: 'Auth token generation failed', type: 'error', details: 'Sorry, we could not generate an authentication token for you. Please try again. If this error message keeps showing, please contact the administrator. Thank you.'},
     'a014': {name: 'Account re-name failed', type: 'error', details: 'The new name is not valid so we cannot use the new name for you.'},
     'a015': { name: 'Invalid user ID', type: 'error', details: 'Please check if you use the correct userID.' },
     'a016': { name: 'Signed out', type: 'success', details: 'You are now signed out.' },
@@ -46,7 +35,6 @@ const messageCodes = {
     'a020': {name: 'Auth token generation failed', type: 'error', details: 'Sorry, we could not generate an authentication token for you. Please try again. If this error message keeps showing, please contact the administrator. Thank you.'},
     'a021': {name: 'Verifiation email sending error', type: 'error', details: 'Sorry, we failed to send you a verification email temporarily.'},
     'a022': {name: 'Account re-name failed', type: 'error', details: 'The new name is not valid so we cannot use the new name for you.'},
-
     '1': { name: 'The email has been used', type: 'error', details: '' },
     '2': { name: 'The email format has errors', type: 'error', details: '' },
     '3': { name: 'The password has errors', type: 'error', details: '' },
@@ -54,66 +42,38 @@ const messageCodes = {
     '5': { name: 'Expired auth token', type: 'error', details: '' },
     '6': { name: 'Email verified', type: 'success', details: '' },
     '7': { name: 'Account signed up', type: 'success', details: '' },
-    '8': { name: 'Account already verified', type: 'warning',
-        details: 'This email has already been verified. ' +
-        'You may sign in to continue your work.'
-    },
+    '8': { name: 'Account already verified', type: 'warning', details: `This email has already been verified. You may sign in to continue your work.`},
     '9': { name: 'Sign in failed', type: 'error', details: '' },
-    '10': { name: 'Welcome!', type: 'info',
-        details: 'This is a platform to anlyze and select' +
-        '<b>biomarker panels</b>. <a href=\'./home\'>This is a google link</a>'
-    },
-    '11': { name: 'Signed in', type: 'success',
-        details: 'Hi, you are now signed in. You may create a project to upload analyze your data,' +
-        'or continue to previous works.'
-    },
-    '12': { name: 'Sign-in required', type: 'warning',
-        details: 'Please sign in first to proceed your work.' },
-    '13': { name: 'File too large', type: 'error',
-        details: 'The file you want to upload is too large. The current file size limit is 8 MB. ' +
-        'If you need to analyze larger files, please contact the web administrator.' },
-    '14': { name: 'File uploaded', type: 'success', details: 'The file has been uploaded successfully.' +
-        'While waiting the format checking, you may continue update the project information.' },
+    '10': { name: 'Welcome!', type: 'info', details: 'This is the next-generation data science workbench'},
+    '11': { name: 'Signed in', type: 'success', details: `Hi, you are now signed in. You may create a project to upload analyze your data, or continue to previous works.`},
+    '12': { name: 'Sign-in required', type: 'warning', details: 'Please sign in first to proceed your work.' },
+    '13': { name: 'File too large', type: 'error', details: 'The file you want to upload is too large. The current file size limit is 8 MB. If you need to analyze larger files, please contact the web administrator.' },
+    '14': { name: 'File uploaded', type: 'success', details: 'The file has been uploaded successfully. While waiting the format checking, you may continue update the project information.' },
     '15': { name: 'Signed out', type: 'success', details: 'You are now signed out.' },
-    '16': { name: 'Invalid file type', type: 'error', details: 'Currently, we only accept the csv file. ' +
-        'Please check the file type you want to upload.' },
-    '17': { name: 'File format checked', type: 'success', details: 'The file you uploaded has been checked. ' +
-        'You may proceed to process the data.' },
-    '18': { name: 'Invalid file format', type: 'error', details: 'We found some problems in your file. ' +
-        'Please check the invalid lines and modify them to upload again.' },
+    '16': { name: 'Invalid file type', type: 'error', details: 'Currently, we only accept the csv file. Please check the file type you want to upload.' },
+    '17': { name: 'File format checked', type: 'success', details: 'The file you uploaded has been checked. You may proceed to process the data.' },
+    '18': { name: 'Invalid file format', type: 'error', details: 'We found some problems in your file. Please check the invalid lines and modify them to upload again.' },
     '19': { name: 'Invalid project id', type: 'error', details: 'We can not find the project for you.' },
     '20': { name: 'File format under checking.', type: 'warning', details: 'The file is under check. Please wait.' },
     '21': { name: 'The outcome column has been specified', type: 'info',
         details: 'The outcome column for the project has been specified for modeling and cannot be changed.' },
     '22': { name: 'Invalid parameters', type: 'error', details: 'You have sent invalid parameters to server.' },
     '23': { name: 'Invalid user ID', type: 'error', details: 'Please check if you use the correct userID.' },
-    '24': { name: 'The project is not ready yet', type: 'warning',
-        details: 'You may process an action for the project which is not ready yet. Please wait until the project is ready.'
-    },
-    '25': { name: 'Project ready', type: 'success',
-        details: 'A project is ready for further analysis. We have created a full datasets for you. ' +
-        'In addition, we also processed the datasets with simple statistics for you. ' +
-        'You may preliminary select some features in or out based on these simple statistics for each feature.'
-    },
+    '24': { name: 'The project is not ready yet', type: 'warning', details: 'You may process an action for the project which is not ready yet. Please wait until the project is ready.'},
+    '25': { name: 'Project ready', type: 'success', details: 'A project is ready for further analysis. We have created a full datasets for you. In addition, we also processed the datasets with simple statistics for you. You may preliminary select some features in or out based on these simple statistics for each feature.'},
     '26': { name: 'Subset ready', type: 'success', details: 'The Data you\'ve just created is ready for further analysis.' },
     '27': { name: 'Invalid data file id', type: 'error', details: 'We can not find the data file for you.' },
-    '28': { name: 'Data name too long', type: 'error', details: 'The name for the data is too long. ' +
-        'Please shorten the name and try saving again.' },
-    '29': { name: 'Data description too long', type: 'error',
-        details: 'The description for the subset is too long. Please shorten the name and try saving again.' },
+    '28': { name: 'Data name too long', type: 'error', details: 'The name for the data is too long. Please shorten the name and try saving again.' },
+    '29': { name: 'Data description too long', type: 'error', details: 'The description for the subset is too long. Please shorten the name and try saving again.' },
     '30': { name: 'Subset removed successfully', type: 'success', details: 'The subset has been removed.' },
-    '31': { name: 'Task not available', type: 'error',
-        details: 'We can not find the task for you. It is not available. Please check if you use the correct taskid' },
+    '31': { name: 'Task not available', type: 'error', details: 'We can not find the task for you. It is not available. Please check if you use the correct taskid' },
     '32': { name: 'Result creation failed', type: 'error', details: 'There are some errors in your input parameters.' },
     '33': { name: 'Task accomplished', type: 'success', details: 'A task has been successfully performed.' },
     '34': { name: 'Task failed', type: 'error', details: 'We have failed to perform the task. Please see if there are error messages.' },
     '35': {name: 'Invalid result ID', type: 'error', details: 'We can not find the result for you.'},
-    '36': { name: 'Result name too long', type: 'error', details: 'The name for the result is too long. ' +
-        'Please shorten the name and try saving again.' },
-    '37': { name: 'Result description too long', type: 'error',
-        details: 'The description for the result is too long. Please shorten the name and try saving again.' },
+    '36': { name: 'Result name too long', type: 'error', details: 'The name for the result is too long. Please shorten the name and try saving again.' },
+    '37': { name: 'Result description too long', type: 'error', details: 'The description for the result is too long. Please shorten the name and try saving again.' },
     '38': { name: 'No privileges', type: 'error', details: 'Sorry, you don\'t have privileges to view the page.'},
-
     // Project related
     '400': {name: 'Failed to create Project', type: 'error', details: 'No privilege to create a project from the package with admin scope'},
     '401': {name: 'Failed to create project', type: 'error', details: 'Sorry, you cannot create a project from utility package.'},
